23b6884c632d4f4844efa21bc50c03fda1150918
Christine Moore <cmoorelvt@gmail.com>
2020-09-14 21:27:07 -0400

    final changes (I hope)

---
+++ questions.js
@@ -0,0 +1,42 @@
+
+var questions = [
+    {
+        title: "Which company developed JS?",
+        choices: ["Google", "Amazon", "Netscape", "Microsoft"],
+        answer: "Netscape"
+    },
+
+    {
+        title: "An array always begins at an index of:",
+        choices: ["-1", "0", "1", "null"],
+        answer: "0"
+    },
+
+    {
+        title: "What is not a data type of JS?",
+        choices: ["Number", "Boolean", "Object", "toothbrush"],
+        answer: "toothbrush"
+    },
+
+    {
+        title: "Which of the following facts about console.log() is false?",
+        choices: ["console.log() is a method", 
+                "console.log() accepts at least one parameter",
+                "console.log() is often used for debugging",
+                "console.log() displays output visible to the user on the page"],
+        answer: "console.log() displays output visible to the user on the page"
+    },
+
+    {
+        title: "What is a function closed with?",
+        choices: ["parentheses", "square brackets", "curly brackets", "quotation marks"],
+        answer: "curly brackets"
+    },
+
+    {
+        title: "What symbol is used for comments in JS?",
+        choices: ["//", ":", "&&", "\\"],
+        answer: "//"
+    },
+];
+
